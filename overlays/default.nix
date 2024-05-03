feaf1214bd42661e0a65012debc4075218d08a93
Percy Timon <percygt.dev@gmail.com>
2024-05-03 11:23:34 +0800

    update

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,24 +2,26 @@
   inputs,
   outputs,
 }: {
-  stash = final: prev:
-    inputs.nixpkgs-stable.legacyPackages.${prev.system}
-    // {
-      inherit (outputs.packages) nixVulkanIntel nixGLIntel wezterm_wrapped wezterm_nightly yaml2nix yazi;
-      inherit (inputs.nix-vscode-extensions.extensions.${prev.system}) vscode-marketplace;
-      vimPlugins =
-        prev.vimPlugins
-        // (import ../packages/vimPlugins.nix {
-          inherit inputs;
-          pkgs = prev;
-        })
-        // {inherit (outputs.packages) codeium-nvim;};
-      tmuxPlugins =
-        prev.tmuxPlugins
-        // (import ../packages/tmuxPlugins.nix) {
-          inherit inputs;
-          pkgs = prev;
-        }
-        // {inherit (outputs.packages) tmuxinoicer;};
-    };
+  default = final: prev: {
+    stash =
+      inputs.nixpkgs-stable.legacyPackages.${prev.system}
+      // {
+        inherit (outputs.packages) nixVulkanIntel nixGLIntel wezterm_wrapped wezterm_nightly yaml2nix yazi;
+        inherit (inputs.nix-vscode-extensions.extensions.${prev.system}) vscode-marketplace;
+        vimPlugins =
+          prev.vimPlugins
+          // (import ../packages/vimPlugins.nix {
+            inherit inputs;
+            pkgs = prev;
+          })
+          // {inherit (outputs.packages) codeium-nvim;};
+        tmuxPlugins =
+          prev.tmuxPlugins
+          // (import ../packages/tmuxPlugins.nix) {
+            inherit inputs;
+            pkgs = prev;
+          }
+          // {inherit (outputs.packages) tmuxinoicer;};
+      };
+  };
 }
