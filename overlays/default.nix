121209d425b8af2e5509849567631f03dab07a3d
Percy Timon <percygt.dev@gmail.com>
2025-04-15 13:39:47 +0800

    update

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,7 +2,6 @@
 {
   default = final: prev: {
     inherit (outputs.packages.${prev.system})
-      clipmon
       simple-completion-language-server
       ;
     inherit (inputs.nix-vscode-extensions.extensions.${prev.system}) vscode-marketplace;
--- packages/default.nix
+++ packages/default.nix
@@ -3,7 +3,6 @@
   inputs,
 }:
 {
-  clipmon = pkgs.callPackage ./clipmon.nix { };
   yaml2nix = inputs.yaml2nix.packages."${pkgs.system}".default;
   tmux-switcher = inputs.tmux-switcher.packages."${pkgs.system}".default;
   simple-completion-language-server =
