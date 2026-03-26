6537f13418277ae16ea110e8daa9b8c81fb91d40
Percy Timon <percygtdev@gmail.com>
2026-03-26 22:41:16 +0800

    update

--- flake.nix
+++ flake.nix
@@ -151,8 +151,13 @@
             };
           };
 
-        # after — pull from nixpkgs lib
-        systems = inputs.flake-parts.lib.defaultSystems;
+        systems = [
+          "x86_64-linux"
+          "x86_64-darwin"
+          "aarch64-linux"
+          "aarch64-darwin"
+        ];
+
       }
     );
 }
